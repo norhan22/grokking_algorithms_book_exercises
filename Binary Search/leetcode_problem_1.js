3eaa57f7f0a8ffc025e7aeb7bab05aaa59f9889e
norhan <nora.moh.2011@gmail.com>
2021-11-07 16:26:59 +0200

    fix output

--- Binary Search/leetcode_problem_1.js
+++ Binary Search/leetcode_problem_1.js
@@ -50,10 +50,34 @@ const binarySearch = function (nums, target) {
       midIndex = Math.floor((minIndex + maxIndex) / 2),
       geuss = nums[midIndex]
     //  Check the target
-    if (geuss === target) return geuss
+    if (geuss === target) return result=  midIndex
     else if (geuss > target) maxIndex = midIndex - 1
     else minIndex = midIndex + 1
   }
 
   return result
 }
+const binarySearchArrayOfObjects = (targetValue, key, sortedArr) {
+      
+      let minIndex = 0, maxIndex = sortedArr.length - 1
+      try {
+        while (minIndex <= maxIndex) {
+          const
+            midIndex = Math.floor((minIndex + maxIndex) / 2),
+            geuss  = arr[midIndex]
+          if (geuss[key] === targetValue) return geuss
+          else if (geuss[key] > targetValue) maxIndex = midIndex - 1
+          else  minIndex = midIndex + 1
+        }
+      } catch (err) {
+        console.log('binarySearchArrayOfObjects', err)
+      }
+
+
+}
+    const myList = [1, 3, 5, 7, 9];
+console.clear();
+console.log("binarySearch", binarySearch(myList, 5)); // 2
+console.log("simpleSearch", simpleSearch(myList, 3)); // 1
+console.log("binarySearch", binarySearch(myList, -1)); // -1
+console.log("simpleSearch", simpleSearch(myList, -1)); // -1
